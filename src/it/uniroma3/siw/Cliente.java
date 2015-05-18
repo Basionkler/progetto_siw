131c0db02ef3d69d4df225e2e9b602d2df5e07f6
Federico <pergdr@yahoo.it>
2015-05-18 15:53:56 +0200

    aggiunto jsf

---
+++ src/it/uniroma3/siw/Cliente.java
@@ -0,0 +1,14 @@
+package it.uniroma3.siw;
+
+import java.util.Date;
+
+public class Cliente {
+	
+	private String nome;
+	private String cognome;
+	private String indirizzo;
+	private String email;
+	private Date dataNascita;
+	private Date dataRegistrazione;
+
+}

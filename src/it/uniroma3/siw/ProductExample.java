7d21095e3a438a2cc8b4f10f1a7f397760734a73
Sparvi <marco.pietrangeli.93@gmail.com>
2015-05-26 11:17:00 +0200

    Classi base
    ho iniziato a inserire un po di classi base

--- src/it/uniroma3/siw/ProductExample.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma3.siw;
-
-public class ProductExample {
-	
-	public void teeest() {
-		System.out.println("This is a test line.");
-	}
-	
-}
---
+++ src/it/uniroma3/siw/model/Product.java
@@ -0,0 +1,81 @@
+package it.uniroma3.siw.model;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Table(name="tb_product")
+@NamedQuery(name = "findAllProducts", query = "SELECT p FROM tb_product p")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Lob
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Float price;
+
+    @OneToOne(mappedBy = "product")
+    private OrderLine orderLine;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Provider> providers;
+
+    
+    //Getter&Setter
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public OrderLine getOrderLine() {
+		return orderLine;
+	}
+
+	public void setOrderLine(OrderLine orderLine) {
+		this.orderLine = orderLine;
+	}
+
+	public List<Provider> getProviders() {
+		return providers;
+	}
+
+	public void setProviders(List<Provider> providers) {
+		this.providers = providers;
+	}
+
+}

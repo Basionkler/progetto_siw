a574b2a3a9537831406fcd6948840d414d3ef70e
Federico <pergdr@yahoo.it>
2015-05-27 15:07:02 +0200

    Effettuata un pò di pulizia, proposta classe di controllo sull'utenza

---
+++ src/it/uniroma3/siw/user/UserController.java
@@ -0,0 +1,9 @@
+package it.uniroma3.siw.user;
+
+import javax.faces.bean.ManagedBean;
+
+/* Da valutare l'idea per il controllo su login, confrontare con "UserBean" */
+@ManagedBean(name = "controllerUser")
+public class UserController {
+		//TODO da implementare?
+}
